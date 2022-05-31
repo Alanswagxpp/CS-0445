9182a5d9f5f30dfde714637104d02ba7af632e7d
Haoshen Wu <95590048+Alanswagxpp@users.noreply.github.com>
2022-05-31 09:59:27 -0400

    Update ArrayBag.java

--- ArrayBag.java
+++ ArrayBag.java
@@ -1,1 +1,16 @@
-
+public class ArrayBag<E> implements BagInterface<E> {
+  private E[] contents;
+  private int size; // This is the number of elements
+  
+  public ArrayBag(int capactiy) {
+//     Type erasure : At runtime, generic types no longer exist
+//     They are replaced with the most general usable type
+//     In this case, at runtime, E becomes Object
+    
+    contents = (E[])new Object[capacity];
+    size = 0;
+  }
+  
+  public boolean add(E entry) {
+    
+  }
