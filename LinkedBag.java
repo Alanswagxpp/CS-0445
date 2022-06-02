7ca0c3692bac1911a1ae29ec3796481dbaa8f161
Haoshen Wu <alanswagxpp@gmail.com>
2022-06-02 10:53:01 -0400

    Update LinkedBag.java

--- LinkedBag.java
+++ LinkedBag.java
@@ -1,12 +1,13 @@
 public class LinkedBag<E> implements BagInterface<E> {
-    
+
     private Node head;
     private int size;
-                                                //why did this 
+    // why did this
 
-// Inner class to store a single Node (an E data reference, and a Node next reference)
-// Becuase this is an inner class, LinkedBag code can access Node contents (data, next) directly without getter / setters
-
+    // Inner class to store a single Node (an E data reference, and a Node next
+    // reference)
+    // Becuase this is an inner class, LinkedBag code can access Node contents
+    // (data, next) directly without getter / setters
 
     private class Node {
         E data;
@@ -16,6 +17,7 @@ public class LinkedBag<E> implements BagInterface<E> {
             this.data = data;
             this.next = null;
         }
+
         Node(E data, Node next) {
             this.data = data;
             this.next = next;
@@ -34,20 +36,19 @@ public class LinkedBag<E> implements BagInterface<E> {
 
     public boolean contains(E entry) {
         boolean result = false;
-        for(Node cur = head; cur!=null && result!=true; cur = cur.next){
+        for (Node cur = head; cur != null && result != true; cur = cur.next) {
             // check contents[i]
-            if(cur.data.equals(entry) ) {
+            if (cur.data.equals(entry)) {
                 result = true;
             }
-        }        
+        }
         return result;
         return getNodeOf(entry) != null;
     }
 
-
     public Node getNodeOf(E entry) {
         Node result = null;
-        for (Node cur = head; cur!= null && result == null ; cur = cur.next) {
+        for (Node cur = head; cur != null && result == null; cur = cur.next) {
             if (cur.data.equals(entry)) {
                 result = null;
             }
@@ -55,5 +56,25 @@ public class LinkedBag<E> implements BagInterface<E> {
         return result;
     }
 
-    
+    public E remove() {
+        E result = null;
+        if (!isEmpty()) {
+            result = head.data;
+            head = head.next;
+            size--;
+        }
+        return result;
+    }
+
+    public boolean delete(E entry) {
+        boolean result = false;
+        Node toDelete = getNodeOf(entry);
+        if (toDelete != null) {
+            toDelete.data = head.data;
+            head = head.next;
+            size--;
+            result = true;
+        }
+        return result;
+    }
 }
