1f7b99a0bba483e98f84c37f41c0554a62f98c9f
Haoshen Wu <alanswagxpp@gmail.com>
2022-06-27 00:17:48 -0400

    Update test.java

--- Test/test.java
+++ Test/test.java
@@ -2,6 +2,6 @@ package Test;
 
 public class test {
     public static void main(String[] args) {
-        System.out.println(5 * (3 + 2));
+        System.out.println(Math.pow(Math.pow(2, 3), 4));
     }
 }
