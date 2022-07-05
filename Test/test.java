83a7e6c8b00a396d16d12a212c3108848fb30f7b
Haoshen Wu <alanswagxpp@gmail.com>
2022-07-05 09:42:38 -0400

    Update test.java

--- Test/test.java
+++ Test/test.java
@@ -3,5 +3,6 @@ package Test;
 public class test {
     public static void main(String[] args) {
         System.out.println(Math.pow(Math.pow(2, 3), 4));
+        System.out.println([1+2.5/2.5]^(35-[6*5.15])+4^2);
     }
 }
