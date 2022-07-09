dd40aa42f9ca8fb565547b1ed4f78d4bcbee583a
Haoshen Wu <alanswagxpp@gmail.com>
2022-07-09 16:11:02 -0400

    1

---
+++ ArrayTest/ArrayTest.java
@@ -0,0 +1,10 @@
+package ArrayTest;
+
+public class ArrayTest {
+    public static void main(String[] args) {
+        int[] testArray = new int[8];
+        if(testArray[8]==0){
+            System.out.println("111");
+        }
+    }
+}
