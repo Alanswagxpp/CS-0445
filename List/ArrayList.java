b33c054de3c0086ca672f979602b099c55b38776
Haoshen Wu <alanswagxpp@gmail.com>
2022-07-12 11:00:09 -0400

    Update ArrayList.java

--- List/ArrayList.java
+++ List/ArrayList.java
@@ -5,5 +5,12 @@ public class ArrayList<E> {
     private E[] contents;
     private int size;
 
-    
+    public ArrayList(int cap) {
+        contents = (E[]) new Object[cap];
+        size = 0;
+    }
+
+    public ArrayList()  {
+        this(12);
+    }
 }
