fb884b7355aff58da3db9d57becd5b51f8994fe0
Haoshen Wu <95590048+Alanswagxpp@users.noreply.github.com>
2022-07-14 09:40:38 -0400

    Update ArrayList.java

--- List/ArrayList.java
+++ List/ArrayList.java
@@ -1,7 +1,7 @@
 package List;
 
 public class ArrayList<E> {
-    
+
     private E[] contents;
     private int size;
 
@@ -10,7 +10,27 @@ public class ArrayList<E> {
         size = 0;
     }
 
-    public ArrayList()  {
+    public ArrayList() {
         this(12);
+    }
+
+    public void add(int newPos, E entry) {
+        if (newPos < 0 || newPos >size) {
+            throw new IndexOutOfBoundsException("Index "+ newPos + " invalid in list of size" + size);
+        }
+        if (newPos < size) {
+            makeRoom(newPos);
+        }
+        contents[newPos] = entry;
+        size++;
+        // This can be done before or after the add, but we have to be consistent
+        if(isFull()) {
+            increaseCapacity();
+        }
+
+    }
+
+    private void makeRoom(int pos) {
+        // Shift items forward to prepare for an insert at newPos
     }
 }
